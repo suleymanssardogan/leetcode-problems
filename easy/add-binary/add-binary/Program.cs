8b49f179d528ad5f6930f4e5c156fdf109fd9197
suleymanssardogan <sardogansuleyman04@gmail.com>
2025-10-28 13:14:48 +0300

    Add easy leetcode solutions: add-binary, merge-two-sorted-lists, plus-one

---
+++ easy/add-binary/add-binary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add_binary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string a = "10";
+            string b = "11";
+            int c  = Convert.ToInt32(b);
+            Console.WriteLine(c);
+
+        }
+
+        public static string AddBinary(string a, string b)
+        {
+            char [] chars = a.ToCharArray();
+            char [] chars2 = b.ToCharArray();
+            char[] result = new char[Math.Max(chars2.Length,chars.Length)];
+            for (int i = chars.Length-1; i >=0; i--)
+            {
+                for (int j = chars2.Length; j>=0; j--)
+                {
+                    if( chars2[j] =='1' && chars[i] == '0')
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ easy/plus-one/plus-one/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace plus_one
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test cases
+            int[] nums = { 1, 2, 9 };
+
+            int[] b = PlusOne(nums);
+
+            for (int i = 0; i<b.Length; i++) 
+            {
+                Console.WriteLine(b[i]);
+            }
+        }
+
+
+        public static int[] PlusOne(int[] digits)
+        {
+
+            int length = digits.Length; 
+
+            for(int i = length - 1; i >= 0; i--)
+            {
+                //travel array from end to begin
+                if(digits[i] < 9)
+                {
+                    digits[i]++;
+                    return digits;
+                }
+                digits[i]=0;
+            }
+
+            //if all digtis are 9 There is carry , create new array
+            int[] result = new int[length + 1];
+            //Put 1 the start ,remaining part of array already 0
+            result[0] = 1;
+            return result;
+ 
+        }
+    }
+}
