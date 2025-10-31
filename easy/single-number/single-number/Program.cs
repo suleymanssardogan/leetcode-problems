5ad72336dea54de2510bc20192423eda74c37b60
suleymanssardogan <sardogansuleyman04@gmail.com>
2025-10-31 15:30:30 +0300

    Single Number solution has added

---
+++ easy/single-number/single-number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace single_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = { 1, 2, 3, 3, 2 };
+            Console.WriteLine($"Single Number is:{SingleNumber(array)} ");
+        }
+
+        public static int SingleNumber(int[] nums)
+        {
+            int result = 0;
+            //Travel every number
+            foreach (var n in nums)
+            {
+                //Bitwise XOR
+                // 1,2,1,2,3
+                // (1^1)^(2^2)^3
+                // 0 ^ 0 ^ 3
+                // 3
+                result = result ^ n;
+            }
+            return result;
+        }
+    }
+}
---
+++ easy/single-number/single-number/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("single-number")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("single-number")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("8757d995-303f-49c5-8e2e-33eb1b312944")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
