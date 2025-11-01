e9152d9124e1a37dcf77e1dca925ca4016f05f58
suleymanssardogan <sardogansuleyman04@gmail.com>
2025-11-02 01:04:40 +0300

    Solution of power of two has added

---
+++ easy/power-of-two/power-of-two/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace power_of_two
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test
+            bool a = IsPowerOfTwo(2);
+            Console.WriteLine(a);
+        }
+
+        public static bool IsPowerOfTwo(int n)
+        {
+            //Less than 0 false
+            if (n<= 0) return false;
+
+            //Based 2 Log
+            double x = Math.Log(n,2);
+            //a whole number control
+            return x % 1 == 0;
+        }
+
+    }
+}
---
+++ easy/power-of-two/power-of-two/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("power-of-two")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("power-of-two")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("b327ec2b-6af5-400d-ab52-9bc949585b00")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
