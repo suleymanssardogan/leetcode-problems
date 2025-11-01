ee0b7e0e0bd6a786c896d6bd417f5919748a492e
suleymanssardogan <sardogansuleyman04@gmail.com>
2025-11-01 11:34:03 +0300

    Majority Element solution has added

---
+++ easy/majority-element/majority-element/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace majority_element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test Case
+            int[] a = { 3, 2, 3 };
+            Console.WriteLine("Majority Element: " + MajorityElement(a));
+        }
+
+        public static int MajorityElement(int[] nums)
+        {
+            //Create dictionary
+            Dictionary<int, int> counts = new Dictionary<int, int>();
+
+            //Travel on the array
+            foreach(int num in nums)
+            {
+                //same element add one every tome
+                if (counts.ContainsKey(num))
+                {
+                    counts[num]++;
+                }
+                //different element always 1
+                else
+                {
+                    counts[num] = 1;
+                }
+
+                //Majority Element Rule
+                if (counts[num] > nums.Length / 2)
+                {
+                    return num;
+                }
+            }
+            // does not exist majority element
+            return -1;
+        }
+    }
+}
---
+++ easy/majority-element/majority-element/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("majority-element")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("majority-element")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("f5af15e4-3f62-491d-ae80-83f6667c0a3c")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
