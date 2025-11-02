a7c46ee94c7f6e62bdf60db5c3f4d906b40acf2b
suleymanssardogan <sardogansuleyman04@gmail.com>
2025-11-02 19:49:40 +0300

    Maximum69Number solution has added

---
+++ easy/maximum-69-number/maximum-69-number/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maximum_69_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 9969;
+            int b = Maximum69Number(a);
+            Console.WriteLine(b);   
+        }
+
+        public static int Maximum69Number(int num)
+        {
+            //Acces to number We can create char array
+            char[] digits = num.ToString().ToCharArray();
+            
+            //Already 9999 is the biggest
+            if (num == 9999)
+            {
+                return num;
+            }
+
+            
+            else
+            {
+                for(int i = 0; i < digits.Length; i++)
+                {
+
+                    //The first 6 has to convert to 9
+                    if(digits[i] == '6')
+                    {
+                        digits[i] = '9';
+                        break;
+                    }
+                }
+            }
+            // convert int
+            return int.Parse(new string(digits));
+        }
+    }
+}
---
+++ easy/maximum-69-number/maximum-69-number/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("maximum-69-number")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("maximum-69-number")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("f091f68b-4483-4ed9-919e-19f79cf7d494")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
